a6607d784193d248f221e692e86ccb818ed133b0
WigoBlah <Vanderweeenn@outlook.com>
2025-09-03 22:08:12 +0200

    Added delete button, some minimal css changes

--- Javascript/To-Do/index.js
+++ Javascript/To-Do/index.js
@@ -1,15 +1,25 @@
 const btnAdd = document.getElementById("btnAdd");
 const inputToDo = document.getElementById("toDoInput");
 const ul = document.getElementById("todoList");
+let delBtn = document.createElement('button');
+delBtn.innerText = "Delete";
+delBtn.id = "btnDel";
 
-function AddToDo(){
+
+btnAdd.onclick = function (){
+    if(inputToDo.value != ""){
     console.log(inputToDo.value);
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(inputToDo.value));
-    li.addEventListener("click", function() {
-        this.classList.toggle("strikethrough");
+    li.addEventListener("click", function(){
+        li.classList.toggle("strikethrough");
     });
+    delBtn.addEventListener("click", function(e) {
+        li.parentNode.removeChild(li);
+    });
+    li.append(delBtn);
     ul.appendChild(li);
     inputToDo.value = "";
+    }
 }
 
