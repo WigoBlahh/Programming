2e1cdc9427bb263e75106d8273df2f2a6d376be6
WigoBlah <Vanderweeenn@outlook.com>
2025-09-04 21:48:16 +0200

    Added full remove and edit functionality

--- Javascript/To-Do/index.js
+++ Javascript/To-Do/index.js
@@ -1,25 +1,52 @@
 const btnAdd = document.getElementById("btnAdd");
-const inputToDo = document.getElementById("toDoInput");
-const ul = document.getElementById("todoList");
-let delBtn = document.createElement('button');
-delBtn.innerText = "Delete";
-delBtn.id = "btnDel";
+const input = document.getElementById("input");
+const list = document.getElementById("list");
 
+let todos = [];
 
-btnAdd.onclick = function (){
-    if(inputToDo.value != ""){
-    console.log(inputToDo.value);
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(inputToDo.value));
-    li.addEventListener("click", function(){
-        li.classList.toggle("strikethrough");
-    });
-    delBtn.addEventListener("click", function(e) {
-        li.parentNode.removeChild(li);
-    });
-    li.append(delBtn);
-    ul.appendChild(li);
-    inputToDo.value = "";
+function AddToDo(){
+    if(input.value != ""){
+        todos.push(input.value);
+        input.value = "";
+        renderList();
     }
 }
 
+function renderList(){
+    list.innerHTML = "";
+        todos.forEach((todo, index) => {
+        //create list item
+        let li = document.createElement('li');
+        //create remove button
+        let btnRemove = document.createElement('button');
+        btnRemove.innerHTML = "Remove";
+        btnRemove.id = "remove";
+        btnRemove.classList = "remove";
+        btnRemove.onclick = function(){
+            todos.splice(index, 1);
+            renderList();
+        }
+        //create edit button
+        let btnEdit = document.createElement('button');
+        btnEdit.innerHTML = "Edit";
+        btnEdit.id = "edit";
+        btnEdit.classList = "edit";
+        btnEdit.onclick = function(){
+            let edit = prompt("Write edited todo");
+            if(edit != ""){
+                todos[index] = edit;
+                console.log(todos);
+                renderList();
+            }else{
+               alert("Edit can't be empty, try again"); 
+            }
+        }
+
+        li.appendChild(document.createTextNode(todo));
+        li.appendChild(btnEdit);
+        li.appendChild(btnRemove);
+        list.appendChild(li);
+    });
+}
+
+btnAdd.onclick = AddToDo;
